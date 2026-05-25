7d8f76c322bb02e0c738a7632bbe62b4d7cf2cf1
Alya2222 <aliaalamry20@gmail.com>
2026-05-25 19:14:40 +0400

    Upload main Java file
    Main Java file for version management task

---
+++ 23S23791_Alya.java
@@ -0,0 +1,47 @@
+
+package jobmarkettrendanalysis;
+import java.util.Scanner;
+public class JobMarketTrendAnalysis {
+    public static void main(String[] args) {
+      Scanner input= new Scanner(System.in);
+      int sectorChoice;
+      int vacancies;
+      System.out.println("Job Market Trend Analysis system");
+      System.out.println("1. IT Sector");
+      System.out.println("2. Healthcare Sector");
+      System.out.println("3. Education Sector");
+      
+      System.out.println("Enter sector choice: ");
+      sectorChoice= input.nextInt();
+      
+      System.out.println("Enter number of vacancies: ");
+      vacancies= input.nextInt();
+      switch (sectorChoice){
+          case 1:
+              if (vacancies >= 100){
+                 System.out.println("Hight demand in IT sector "); 
+              } else {
+                  System.out.println("Low demad in IT sector");
+              }
+              break;
+          case 2:
+              if (vacancies >= 80){
+                 System.out.println("Hight demand in Healthcare sector "); 
+              } else {
+                  System.out.println("Low demad in Healthcare sector");
+              }
+              break;
+           case 3:
+              if (vacancies >= 60){
+                 System.out.println("Hight demand in Education sector "); 
+              } else {
+                  System.out.println("Low demad in Education sector");
+              }
+              break;
+           default:
+              System.out.println("Invalid sector choice");        
+      }
+      input.close();
+    }
+    
+}
